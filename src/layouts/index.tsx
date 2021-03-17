ac3d8f02c687f2bccd1a830cfa04b4adc3a3f848
xuwu <zx597813039@gmail.com>
2021-03-18 07:41:37 +0800

    feat: use antd

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import React, { useState, useCallback, useEffect } from 'react';
+import { Tabs } from 'antd';
 import { IRouteComponentProps } from 'umi';
+
+const { TabPane } = Tabs;
 
 export default function Layout({
   children,
@@ -9,25 +10,26 @@ export default function Layout({
   route,
   history,
 }: IRouteComponentProps) {
-  const pathName = location.pathname === '/' ? '/questions' : location.pathname;
-  const [value, setValue] = useState(pathName);
+  const [value, setValue] = useState('');
 
-  const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
+  useEffect(() => {
+    if (location.pathname === '/') {
+      setValue('/questions');
+      history.push('/questions');
+    }
+  }, []);
+
+  const handleChange = useCallback((newValue: string) => {
     setValue(newValue);
     history.push(newValue);
-  };
+  }, []);
+
   return (
     <div>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        indicatorColor="primary"
-        textColor="primary"
-        centered
-      >
-        <Tab label="十五万字面试资料" value="/interview" />
-        <Tab label="每日大厂原题" value="/questions" />
-        <Tab label="大厂内推信息" value="jobs" />
+      <Tabs activeKey={value} onChange={handleChange} centered>
+        <TabPane tab="十五万字面试资料" key="/interview" />
+        <TabPane tab="每日大厂原题" key="/questions" />
+        <TabPane tab="大厂内推信息" key="/jobs" />
       </Tabs>
       {children}
     </div>
--- src/pages/questions.tsx
+++ src/pages/questions.tsx
@@ -1,13 +1,5 @@
 import React, { useEffect, useCallback, useState } from 'react';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import TablePagination from '@material-ui/core/TablePagination';
-import Skeleton from '@material-ui/lab/Skeleton';
+
 import styled from 'styled-components';
 import { getQuestionList } from '@/api';
 import { IQuestion } from '@/types';
@@ -23,23 +15,13 @@ const StyledContainer = styled.div`
   }
 `;
 
-function createData(name: string, calories: number, fat: number) {
-  return { name, calories, fat };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0),
-  createData('Ice cream sandwich', 237, 9.0),
-  createData('Eclair', 262, 16.0),
-];
-
 export default function BasicTable() {
   const [page, setPage] = React.useState(0);
   const [listData, setListData] = useState<IQuestion[]>([]);
 
   const fetchData = useCallback(async () => {
     const listData = await getQuestionList();
-    // setListData(listData)
+    setListData(listData);
   }, []);
 
   useEffect(() => {
@@ -49,48 +31,5 @@ export default function BasicTable() {
   const handleChangePage = (event: unknown, newPage: number) => {
     setPage(newPage);
   };
-  return (
-    <StyledContainer>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>题名</TableCell>
-              <TableCell>分类</TableCell>
-              <TableCell>公司</TableCell>
-            </TableRow>
-          </TableHead>
-          {listData.length ? (
-            <TableBody>
-              {listData.map((row) => (
-                <TableRow key={row.issueId}>
-                  <TableCell component="th" scope="row">
-                    {row.name}
-                  </TableCell>
-                  <TableCell>{row.type}</TableCell>
-                  <TableCell>{row.company}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          ) : (
-            <>
-              <Skeleton animation="wave" height={53} width="100%" />
-              <Skeleton animation="wave" height={53} width="100%" />
-              <Skeleton animation="wave" height={53} width="100%" />
-            </>
-          )}
-        </Table>
-      </TableContainer>
-      {listData.length ? (
-        <TablePagination
-          rowsPerPage={20}
-          rowsPerPageOptions={[]}
-          count={listData.length}
-          page={page}
-          onChangePage={handleChangePage}
-          component="div"
-        />
-      ) : null}
-    </StyledContainer>
-  );
+  return <StyledContainer>1</StyledContainer>;
 }
