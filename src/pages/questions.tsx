d02e1ec93e3898f83a3fd109413013a08f0628b7
KieSun <zx597813039@gmail.com>
2021-03-18 16:52:13 +0800

    feat: question list

--- src/pages/questions.tsx
+++ src/pages/questions.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useCallback, useState } from 'react';
-
+import { Tag, Table } from 'antd';
+import { Link } from 'umi';
 import styled from 'styled-components';
 import { getQuestionList } from '@/api';
 import { IQuestion } from '@/types';
@@ -18,18 +19,62 @@ const StyledContainer = styled.div`
 export default function BasicTable() {
   const [page, setPage] = React.useState(0);
   const [listData, setListData] = useState<IQuestion[]>([]);
+  const [loading, setLoading] = useState(true);
 
   const fetchData = useCallback(async () => {
-    const listData = await getQuestionList();
-    setListData(listData);
+    try {
+      const listData = await getQuestionList();
+      setListData(listData);
+    } finally {
+      setLoading(false);
+    }
   }, []);
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  const columns = [
+    {
+      title: '题名',
+      dataIndex: 'name',
+      key: 'name',
+      render: (name: string) => <Link to="/questions/detail">{name}</Link>,
+    },
+    {
+      title: '类型',
+      key: 'type',
+      dataIndex: 'type',
+      render: (type: string[]) => (
+        <>
+          {type &&
+            type.map((tag) => {
+              let color = tag.length > 5 ? 'geekblue' : 'green';
+              if (tag === 'loser') {
+                color = 'volcano';
+              }
+              return (
+                <Tag color={color} key={tag}>
+                  {tag.toUpperCase()}
+                </Tag>
+              );
+            })}
+        </>
+      ),
+    },
+    {
+      title: '公司',
+      dataIndex: 'company',
+      key: 'company',
+    },
+  ];
+
   const handleChangePage = (event: unknown, newPage: number) => {
     setPage(newPage);
   };
-  return <StyledContainer>1</StyledContainer>;
+  return (
+    <StyledContainer>
+      <Table loading={loading} columns={columns} dataSource={listData} />
+    </StyledContainer>
+  );
 }
